03d1b134396c10fb10891a89e6157f7c3057e4f3
FemBlack <riazmohamedd@gmail.com>
2013-08-17 00:20:45 +0530

    test

--- XblastTools - Copy/src/ind/fem/black/xposed/adapters/IIconListAdapterItem.java
+++
@@ -1,8 +0,0 @@
-package ind.fem.black.xposed.adapters;
-
-import android.graphics.drawable.Drawable;
-
-public interface IIconListAdapterItem extends IBaseListAdapterItem {
-    public Drawable getIconLeft();
-    public Drawable getIconRight();
-}

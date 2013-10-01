3e36e032845a2c34da18d2a680c53d5788633517
FemBlack <riazmohamedd@gmail.com>
2013-10-01 23:20:44 +0200

    Update MoveApptoDataFolderDialog.java

--- src/ind/fem/black/xposed/dialogs/MoveApptoDataFolderDialog.java
+++ src/ind/fem/black/xposed/dialogs/MoveApptoDataFolderDialog.java
@@ -1,64 +1,6 @@
 package ind.fem.black.xposed.dialogs;
 
-import java.util.List;
 
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
-import android.app.ProgressDialog;
-import android.content.DialogInterface;
-import android.os.Bundle;
-import android.widget.Toast;
-
-import com.ramdroid.roottools.ex.AsyncShell;
-import com.ramdroid.roottools.ex.ErrorCode;
-
-public class MoveApptoDataFolderDialog extends DialogFragment
-{
-	public static MoveApptoDataFolderDialog newInstance()
-	  {
-		MoveApptoDataFolderDialog localPromptMoveAppDialog = new MoveApptoDataFolderDialog();
-	    localPromptMoveAppDialog.setArguments(new Bundle());
-	    return localPromptMoveAppDialog;
-	  }
-	
-  public Dialog onCreateDialog(Bundle paramBundle)
-  {
-    setCancelable(false);
-    return new AlertDialog.Builder(getActivity()).setTitle("Move XBlast Tools to Internal Storage").setMessage("App needs to be moved to internal storage to work properly.\n\n(Start the app again after you select Move and the app closes)").setCancelable(false).setPositiveButton("Move", new DialogInterface.OnClickListener()
-    {
-      public void onClick(DialogInterface dialogInterface, int value)
-      {
-        try
-        {
-          ProgressDialog.show(MoveApptoDataFolderDialog.this.getActivity(), "", "Moving XBlast Tools", true);
-          String str1 = "cat " + MoveApptoDataFolderDialog.this.getActivity().getApplicationInfo().sourceDir + " > " + MoveApptoDataFolderDialog.this.getActivity().getExternalCacheDir().getAbsolutePath() + "/tmp.apk";
-          String str2 = "pm install -r -f " + MoveApptoDataFolderDialog.this.getActivity().getExternalCacheDir().getAbsolutePath() + "/tmp.apk";
-         
-          AsyncShell.send(true, str1 + " && " + str2, new ErrorCode.OutputListener()
-          {
-            public void onResult(int paramAnonymous2Int, List<String> paramAnonymous2List) {
-            	
-            }
-          });
-          return;
-        }
-        catch (Exception localException)
-        {
-          localException.printStackTrace();
-          Toast.makeText(MoveApptoDataFolderDialog.this.getActivity(), "Failed to move app to internal storage!", Toast.LENGTH_SHORT).show();
-          MoveApptoDataFolderDialog.this.getActivity().finish();
-        }
-      }
-    }).setNegativeButton("Quit", new DialogInterface.OnClickListener()
-    {
-      public void onClick(DialogInterface paramAnonymousDialogInterface, int paramAnonymousInt)
-      {    	 
-        MoveApptoDataFolderDialog.this.getActivity().finish();
-        MoveApptoDataFolderDialog.this.dismiss();
-      }
-    }).create();
-  }
   
   /*@Override
   public void onDetach() {
